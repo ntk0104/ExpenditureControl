17e8c921444bec9b7138e6b811090e2f6c7d3859
Kiet Nguyen <roynguyen147@gmail.com>
2018-11-18 23:52:10 +0700

    create/delete group expenditure

--- app/src/main/java/com/roynguyen147/expenditurecontrol/Database.java
+++ app/src/main/java/com/roynguyen147/expenditurecontrol/Database.java
@@ -4,19 +4,23 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 public class Database extends SQLiteOpenHelper {
+
+    MainActivity context;
 
     public Database(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     // Impact database function
-    public void QueryData(String sql){
+    public void QueryData(String sql) {
         SQLiteDatabase database = getWritableDatabase();
         database.execSQL(sql);
-    }
 
+    }
     // Get-data only function (Non-impact database function)
     public Cursor GetData(String sql){
         SQLiteDatabase database = getReadableDatabase();
@@ -33,14 +37,50 @@ public class Database extends SQLiteOpenHelper {
 
     }
 
-    public void createDBForTheFirstTime(){
-        // Create table Group if its not exists
-        String group_creation_sql = "CREATE TABLE IF NOT EXISTS group_expenditure(id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(200) NOT NULL)";
-        QueryData(group_creation_sql);
+    // Create table group_expenditure for the first time application run
+    public boolean createDBForTheFirstTime(){
+        try {
+            // Create table Group if its not exists
+            String group_creation_sql = "CREATE TABLE IF NOT EXISTS `group_expenditure` (\n" +
+                    "\t`id`\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+                    "\t`title`\tTEXT NOT NULL UNIQUE\n" +
+                    ");";
+
+            QueryData(group_creation_sql);
+            // Create table group_expenditure successfully
+            return true;
+        }catch (Exception e){
+            // Create table group_expenditure failed
+            return false;
+        }
+
     }
 
-    public void addNewGroup(String new_group_title){
-        String add_new_group_sql = "INSERT INTO group_expenditure VALUES(null, '" + new_group_title + "')";
-        QueryData(add_new_group_sql);
+    // Add new group of expenditure - unique value
+    public boolean addNewGroup(String new_group_title){
+        try {
+            String add_new_group_sql = "INSERT INTO group_expenditure VALUES(null, '" + new_group_title + "')";
+            QueryData(add_new_group_sql);
+            // Create new group of expenditure successfully
+            return true;
+        }catch (Exception e){
+            // Create new group of expenditure failed
+            return false;
+        }
+
+    }
+
+    // Delete the group of expenditure
+    public boolean DeleteGroup(String group_title){
+        try {
+            String add_new_group_sql = "DELETE FROM `group_expenditure` WHERE `title` = '" + group_title + "';";
+            QueryData(add_new_group_sql);
+            // Delete group of expenditure successfully
+            return true;
+        }catch (Exception e){
+            // Delete new group of expenditure failed
+            return false;
+        }
+
     }
 }
